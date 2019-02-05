3125af5ea64fee6c85a6b559fd90e2f7296aea38
Calvin Hu <ycalvinhu@gmail.com>
2019-02-04 19:38:41 -0800

    Adding a verification helper

--- blockchain.py
+++ blockchain.py
@@ -3,9 +3,10 @@ import hashlib as hl
 import json
 import pickle
 
-from hash_util import hash_string_256, hash_block
+from hash_util import hash_block
 from block import Block
 from transaction import Transaction
+from verification import Verification
 
 # The reward we give to miners (for creating a new block)
 MINING_REWARD = 10
@@ -105,19 +106,12 @@ def save_data():
         print("Saving failed!")
 
 
-def valid_proof(transactions, last_hash, proof):
-    guess = (
-        str([tx.to_ordered_dict for tx in transactions]) + str(last_hash) + str(proof)
-    ).encode()
-    guess_hash = hash_string_256(guess)
-    return guess_hash[0:2] == "00"
-
-
 def proof_of_work():
     last_block = blockchain[-1]
     last_hash = hash_block(last_block)
     proof = 0
-    while not valid_proof(open_transactions, last_hash, proof):
+    verifier = Verification()
+    while not verifier.valid_proof(open_transactions, last_hash, proof):
         proof += 1
     return proof
 
@@ -166,16 +160,6 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def verify_transaction(transaction):
-    """Verify a transaction by checking whether the sender has sufficient coins.
-
-    Arguments:
-        :transaction: The transaction that should be verified.
-    """
-    sender_balance = get_balance(transaction.sender)
-    return sender_balance >= transaction.amount
-
-
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
@@ -190,7 +174,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         :amount: The amount of coins sent with the transaction (default = 1.0)
     """
     transaction = Transaction(sender, recipient, amount)
-    if verify_transaction(transaction):
+    verifier = Verification()
+    if verifier.verify_transaction(transaction, get_balance):
         open_transactions.append(transaction)
         save_data()
         return True
@@ -239,24 +224,6 @@ def print_blockchain_elements():
         print("-" * 20)
 
 
-def verify_chain():
-    """ Verify the current blockchain and return True if it's valid, False otherwise."""
-    for (index, block) in enumerate(blockchain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print("Proof od work is invalid")
-            return False
-    return True
-
-
-def verify_transactions():
-    """Verifies all open transactions."""
-    return all([verify_transaction(tx) for tx in open_transactions])
-
-
 waiting_for_input = True
 
 # A while loop for the user input interface
@@ -285,7 +252,8 @@ while waiting_for_input:
     elif user_choice == "3":
         print_blockchain_elements()
     elif user_choice == "4":
-        if verify_transactions():
+        verifier = Verification()
+        if verifier.verify_transactions(open_transactions, get_balance):
             print("All transactions are valid")
         else:
             print("There are invalid transactions")
@@ -294,7 +262,8 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
-    if not verify_chain():
+    verifier = Verification()
+    if not verifier.verify_chain(blockchain):
         print_blockchain_elements()
         print("Invalid blockchain!")
         # Break out of the loop
